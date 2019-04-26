1142a8aca03fc4584989c6d1b4c2d369c08665d8
Can Tuksavul <ctuksavul@mosys.com>
2019-04-26 15:52:23 -0700

    updated .vim and added script to build vim from source

---
+++ build_vim_with_python.sh
@@ -0,0 +1,1 @@
+./configure --with-features=huge --enable-multibyte --enable-rubyinterp=yes --enable-pythoninterp=yes --with-python-config-dir=/usr/lib/python2.6/config --enable-perlinterp=yes --enable-luainterp=yes --enable-gui=gtk2 --enable-cscope --prefix=/home/ctuksavul/myvim
