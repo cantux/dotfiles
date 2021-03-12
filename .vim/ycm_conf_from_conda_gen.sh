3e5e15181521ae18e3d8df1afcc2042a5b3a0108
cantux <can.tuksavul@gmail.com>
2021-03-11 18:18:20 -0800

    ycm conf gen prob

--- .vim/ycm_conf_from_conda_gen.sh
+++ .vim/ycm_conf_from_conda_gen.sh
@@ -1,13 +1,14 @@
-
+#!/bin/bash
 
 external_modules="$(echo $CONDA_PREFIX/lib/python*)"
 
-echo "def Settings( **kwargs ):
+cat > .ycm_extra_conf.py << EOL
+def Settings( **kwargs ):
   return {
     'sys_path': [
       '${external_modules}'
       ],
     'interpreter_path': '${CONDA_PYTHON_EXE}'
-  }" \ 
-      > .ycm_extra_conf.py
+  }
+EOL
 
